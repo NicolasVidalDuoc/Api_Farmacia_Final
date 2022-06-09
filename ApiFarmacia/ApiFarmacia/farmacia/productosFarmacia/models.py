e534a18838c69d4a1f52ac643d3e24e9fff8f8a8
NicolasVidalDuoc <86321476+NicolasVidalDuoc@users.noreply.github.com>
2022-06-09 19:54:28 -0400

    Se cambia MaxValueValidator de 100 a 1000 en id_producto y stock_producto

--- ApiFarmacia/ApiFarmacia/farmacia/productosFarmacia/models.py
+++ ApiFarmacia/ApiFarmacia/farmacia/productosFarmacia/models.py
@@ -5,13 +5,10 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Productos(models.Model):
     id_producto = models.IntegerField(primary_key=True, default=1, 
-                    validators=[MaxValueValidator(100), MinValueValidator(1)])
+                    validators=[MaxValueValidator(1000), MinValueValidator(1)])
     nombre_producto = models.CharField(max_length=60)
     stock_producto = models.IntegerField(default=1, 
-                    validators=[MaxValueValidator(100), MinValueValidator(1)])
+                    validators=[MaxValueValidator(1000), MinValueValidator(1)])
 
     def __str__(self):
         return self.nombre_producto
-
-
-
